f7c15b94ce352d5eb12dde352f11e72d41adf0f3
Pri-21-coder <prithapal114@gmail.com>
2026-06-10 13:46:59 +0530

    fix case sensitivity in css import

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import SunnyIcon from '@mui/icons-material/Sunny';
-import "./infoBox.css";
+import "./InfoBox.css";
 export default function InfoBox({info}){
     const INIT_URL="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1075";
     const HOT_URL="https://images.unsplash.com/photo-1504370805625-d32c54b16100?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332";
